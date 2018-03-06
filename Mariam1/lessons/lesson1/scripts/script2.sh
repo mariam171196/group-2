906647f7b7aca6ce618f18e24c32519712f1dc27
Mariam Karapetyan <karapetyan.mariam21@gmail.com>
2018-03-06 21:53:36 +0400

    cleanup done by Aghasi for Mariam

---
+++ Mariam1/lessons/lesson1/scripts/script2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cd /workspace/Mariam1/lessons/lesson1/output
+echo "u1:a.txt:"
+cd /workspace/Mariam1/lessons/lesson1/output/u1
+cat a.txt
+echo "u3:a.txt:"
+cd /workspace/Mariam1/lessons/lesson1/output/u1/u2/u3
+cat a.txt
+echo "u3:b.txt:"
+cd /workspace/Mariam1/lessons/lesson1/output/u1/u2/u3
+cat b.txt
---
+++ Mariam1/lessons/lesson1/scripts/script3.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+cd /workspace/Mariam1/lessons/lesson1/output
+rm -rf u1
+mkdir u1
+cd u1
+mkdir u2
+cd u2
+mkdir u3
+mkdir u4
+cd u4
+> a.txt
+>b.txt
+
