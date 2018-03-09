c000318a81b08491fbf0c8ed7e3023bbc1f7cd31
Mariam Karapetyan <itcstepanakertmariam@gmail.com>
2018-03-09 12:46:41 +0000

    Homework

---
+++ Mariam1/scripts/script1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+cd ~/Desktop/Mariam/lessons/lesson1/output
+rm -r u1
+mkdir u1
+cd u1
+mkdir u2
+> a.txt
+cd u2
+mkdir u3
+mkdir u4
+cd u3
+> a.txt
+> b.txt
+
---
+++ Mariam1/scripts/script2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+cd ~/Desktop/Mariam/lessons/lesson1/output
+echo "u1:a.txt:"
+cd ~/Desktop/Mariam/lessons/lesson1/output/u1
+cat a.txt
+echo "u3:a.txt:"
+cd ~/Desktop/Mariam/lessons/lesson1/output/u1/u2/u3
+cat a.txt
+echo "u3:b.txt:"
+cd ~/Desktop/Mariam/lessons/lesson1/output/u1/u2/u3
+cat b.txt
+
