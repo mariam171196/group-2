9c4763d5b9b6e0ad8904b434272809b0788ea0d7
Mariam <Karapetyan.Mariam21@gmail.com>
2018-03-20 18:48:55 +0400

    Classwork C++

---
+++ Mariam1/C++ Projects/Age.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(){
+
+
+
+int age;
+std::cout<< "Hello, how old are you?:\n";
+std::cin>> age;
+std::cout<< "You are " << age<< " years old!";
+
+
+
+return 0;
+
+}
---
+++ Mariam1/C++ Projects/OurAge.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+
+int main(){
+
+std::cout << "Hello!!!";
+
+int age_of_user;
+int age_of_pair;
+std::string  user_name;
+std::string  pair_name;
+
+std::cout <<"What's your name?: \n";
+std::cin>> user_name;
+std::cout <<"How old are you?: \n";
+std::cin>> age_of_user;
+
+
+std::cout <<"What's your pair's name?: \n";
+std::cin>>pair_name;
+std::cout <<"How old are your pair?: \n";
+std::cin>>age_of_pair;
+
+if (age_of_user > age_of_pair){
+std::cout<< user_name << ">" << pair_name;
+}
+else if (age_of_pair>age_of_user){
+std::cout<< pair_name << ">" << user_name;
+}
+else {
+std::cout<< pair_name<< "=" <<user_name;
+}
+
+return 0;
+
+}
